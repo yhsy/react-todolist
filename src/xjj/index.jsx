dee4938f16451936331ff54bbc2421a2e0831583
fishku <juerui@vip.qq.com>
2020-02-15 15:25:23 +0800

    2-小姐姐组件-新增服务功能

--- src/xjj/index.jsx
+++ src/xjj/index.jsx
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 class XiaoJieJie extends Component {
+  // JS的构造函数,
+  constructor(props) {
+    // 固定写法,调用父类的构造函数
+    super(props)
+    this.state = {
+      // input输入框的值
+      inputValue: '',
+      // 服务列表
+      list: ['头部按摩', '精油推背']
+    }
+  }
+
   render () {
     return (
       <div>
-        <div><input /> <button> 增加服务 </button></div>
+        <div>
+          <input
+            value={this.state.inputValue}
+            onChange={this.inputChange.bind(this)}
+            placeholder="请输入服务内容"
+          />
+          <button onClick={this.addService.bind(this)}> 增加服务 </button>
+        </div>
         <ul>
-          <li>头部按摩</li>
-          <li>精油推背</li>
+          {
+            this.state.list.map((item, index) => {
+              return <li key={index + item}>{item}</li>
+            })
+          }
         </ul>
       </div>
     );
+  }
+
+  // 输入监听
+  inputChange (e) {
+    console.log(e.target.value)
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  // 新增服务
+  addService () {
+    this.setState({
+      list: [...this.state.list, this.state.inputValue]
+    })
   }
 }
 
