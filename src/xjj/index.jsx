07c5e67a8bc640a0f1e58940ef2bb9836bafb429
fishku <juerui@vip.qq.com>
2020-02-15 14:54:25 +0800

    2-新建"小姐姐"组件,基础样式调整

---
+++ src/xjj/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class XiaoJieJie extends Component {
+  render () {
+    return (
+      <div>
+        <div><input /> <button> 增加服务 </button></div>
+        <ul>
+          <li>头部按摩</li>
+          <li>精油推背</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default XiaoJieJie;
